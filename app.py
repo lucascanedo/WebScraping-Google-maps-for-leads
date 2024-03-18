4c0ef2ff42db04e4c6782856b1be6fa555c1049d
Lucas Canedo <saleslucas12@gmail.com>
2024-03-18 02:27:01 -0300

    Mudança total do codigo, utilizando soup e regex. Ate aqui funcionou tudo.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from lxml import html
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,8 +7,7 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 
-
-Loja = input("Escreva o nome do estabelecimento: ")
+Loja = str(input("Escreva o nome do estabelecimento: "))
 Numero_Lojas = int(input("Escreva a quantidade de lojas pesquisadas: "))
 
 driver = webdriver.Firefox()
@@ -25,91 +26,87 @@ soup = BeautifulSoup(driver.page_source, 'lxml')
 
 lista = []
 
-for i in range(2,len(str(soup).split('hfpxzc')) -1):
-    lista.append(str(soup).split('hfpxzc')[i].split('href="')[1].split('" ')[0])
+# Encontrando os links dos estabelecimentos
+
+for tag in soup.find_all("a", href=True):
+    if "/place/" in tag['href']:
+        lista.append(tag['href'])
+        if len(lista) == Numero_Lojas:
+            break
+    
 
 ################################ Pegando os Post's
 
-driver = webdriver.Firefox()
-
-nome_unidade = []
+Nome = []
 nota = []
-loja = []
+#loja = []
 avaliacoes = []
-localizacao = []
 site = []
 telefone = []
-    
-for i in range(0,len(lista[:Numero_Lojas])):
-    
-    driver.get(lista[i])
-    time.sleep(4)
 
-    # Pegando a flag presente nos posts 
+for i, url in enumerate(lista):
+
+    driver.get(url)
+    time.sleep(6)
+
+    # Pegando a flag presente nos posts
 
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    
-    # Alocando o conteudo em memória por meio de lista para otimizar 
 
-    try:
-        nome_unidade.append(str(soup).split('<title>')[1].split('</title>')[0])
-    except:
-        nome_unidade.append(' ')
-           
-    try:
-        nota.append(str(soup).split('class="F7nice"')[1].split('</span>')[0].rsplit('>')[-1])
-    except:
-        nota.append(' ')
-            
-    try:
-        loja.append(str(soup).split('class="DkEaL"')[1].split('</button>')[0].rsplit('>')[-1])
-    except:
-        loja.append(' ')
-        
-        
-    try:
-        avaliacoes.append(str(soup).split('class="rFrJzc UpDOYb"></span></span>')[1].split(' avaliações')[0].split('label="')[1])
-    except:
-        avaliacoes.append(' ')
+    # Alocando o conteudo em memória por meio de lista para otimizar
 
-    try:
-        localizacao.append(str(soup).split('Io6YTe fontBodyMedium kR99db')[1].split('</div>')[0].split('">')[1])
-    except:
-        localizacao.append(' ')
-        
+    # Pegando o título do estabelecimento
+    Nome_loja = soup.find("h1", class_="DUwDvf lfPIob").text if soup.find("h1", class_="DUwDvf lfPIob") else ' '
+    Nome.append(Nome_loja)
+
+    # Pegando a nota do estabelecimento
+    nota_valor = soup.find("span", class_="MW4etd").text if soup.find("span", class_="MW4etd") else ' '
+    nota.append(nota_valor)
+
+    # Pegando o nome do estabelecimentoF
+    #loja_tipo = soup.find("button", class_="DkEaL ").text if soup.find("button", class_="DkEaL ") else ' '
+    #loja.append(loja_tipo)
+
+    # Pegando as avaliações
+    avaliacoes_valor = soup.find("span", class_="UY7F9").text if soup.find("span", class_="UY7F9") else ' '
+    avaliacoes.append(avaliacoes_valor)
+
     # Verificando o site
     site_value = 'No Link'
-    for index in range(2, 8):
-        try:
-            site_value = str(soup).split('Io6YTe fontBodyMedium kR99db')[index].split('</div>')[0].split('">')[1]
-            if ".com" in site_value:
-                break  # Interrompe o loop se encontrar um link válido
-        except:
-            pass
-    site.append(site_value if ".com" in site_value else 'No Link')
+    for tag in soup.find_all("a", href=True):
+        aria_label = tag.get('aria-label', '')
+        if aria_label and 'Website:' in aria_label:
+            site_value = tag['href']
+            break
+    site.append(site_value)
+
 
     # Verificando o telefone
     telefone_value = 'No Phone'
-    for index in range(2, 7):
-        try:
-            telefone_value = str(soup).split('Io6YTe fontBodyMedium kR99db')[index].split('</div>')[0].split('">')[1]
-            if any(char.isdigit() for char in telefone_value):
-                break  # Interrompe o loop se encontrar um número de telefone válido
-        except Exception as e:
-                print(e)
+    for tag in soup.find_all("div", class_="Io6YTe fontBodyMedium kR99db"):
+        telefone_match = re.search(r'(\(?\d{2}\)?\s?)?(\d{4,5}-\d{4})', tag.text)
+        if telefone_match:
+            telefone_value = telefone_match.group()
+            break
     telefone.append(telefone_value)
-        
+
+
 driver.quit()
 
 # Estruturando um Dataframe
 
-df = pd.DataFrame (lista ,columns=['Url'])
-df['nome_unidade'] = nome_unidade
+df = pd.DataFrame(lista, columns=['Url'])
+df['Nome_loja'] = Nome
 df['nota'] = nota
-df['loja'] = loja
+#df['loja_tipo'] = loja
 df['avaliacoes'] = avaliacoes
-df['localizacao'] = localizacao
 df['site'] = site
 df['telefone'] = telefone
 
 print(df)
+df.info()
+# Salvando o DataFrame em um arquivo CSV
+#df.to_csv('dados_estabelecimentos.csv', index=False)
+
+#print("Os dados foram salvos em dados_estabelecimentos.csv")
+
